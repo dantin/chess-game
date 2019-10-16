425a385e11a5c9dd19913f83ac97eef394719ffd
dantin <chengjie.ding@gmail.com>
2019-10-16 21:49:12 +0800

    fix bug and adjust tox.

--- chess/__init__.py
+++ chess/__init__.py
@@ -11,7 +11,7 @@ A library which is used for chess game GIF generation.
 import os
 
 
-__version__ = '1.0.0-git'
+__version__ = '1.0.0.dev0'
 
 # chesspieces
 KING, QUEEN, BISHOP, KNIGHT, ROCK, PAWN = 'k', 'q', 'b', 'n', 'r', 'p'
--- setup.py
+++ setup.py
@@ -11,7 +11,10 @@ with io.open('README.md', 'rt', encoding='utf8') as f:
     README = f.read()
 
 with io.open('chess/__init__.py', 'rt', encoding='utf8') as f:
-    VERSION = re.search(r'__version__ = \'(.*?)\'', f.read()).group()
+    VERSION = re.search(r'__version__ = \'(.*?)\'', f.read()).group(1)
+
+with io.open('requirements/deps.txt', 'rt', encoding='utf8') as f:
+    install_requires = [s for s in f.read().splitlines() if not s.startswith('#') and s != '']
 
 setup(
     name='chess',
@@ -23,13 +26,5 @@ setup(
     url='https://github.com/dantin/chess-game',
     license='BSD',
     packages=find_packages(exclude=('tests', 'misc', 'htmlcov', '*.egg-info')),
-    install_requires=[
-        'imageio>=2.6.1',
-        'numpy>=1.17.2',
-    ],
-    extras_require={
-        'test': [
-            'tox>=3.14.0',
-        ],
-    },
+    install_requires=install_requires,
 )
